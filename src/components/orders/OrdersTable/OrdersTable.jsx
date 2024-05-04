bbdea9584118b866e9a075f3e6061e0ed0759c76
DmiTsa <dmitrytaskaev.dt@gmail.com>
2024-05-04 11:04:25 +0300

    integrate Modal in OrderTable

--- src/components/orders/OrdersTable/OrdersTable.jsx
+++ src/components/orders/OrdersTable/OrdersTable.jsx
@@ -1,22 +1,40 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import { constructionsData } from "../../../data/constructionsData.js";
-import stringMatcher from "../../../utils/stringMatcher.js";
 import Construction from "../Construction/Construction.jsx";
+import Modal from "../../../partsComponent/Modal/Modal.jsx";
+import ConstructionInfo from "../ConstructionInfo/ConstructionInfo.jsx";
 import { selectFilterConstructions } from "../../../redux/orders/filterOrderSlice.js";
-
+import { selectConstructions } from "../../../redux/orders/constructionSlice.js";
+import stringMatcher from "../../../utils/stringMatcher.js";
 import style from "./OrdersTable.module.css";
 
 export default function OrdersTable() {
+  const [modalActive, setModalActive] = useState(false);
+  const [constructionModal, setConstructionModal] = useState({});
   const matchConstruction = useSelector(selectFilterConstructions);
+  const constructionsData = useSelector(selectConstructions);
+
+  const nStyle = style.orderTableCell + " " + style.cellBold;
 
   const filtredData = constructionsData.filter((construction) =>
     stringMatcher(matchConstruction, construction.name)
   );
 
-  const nStyle = style.orderTableCell + " " + style.cellBold;
+  const constructionNameClickHandler = (id) => {
+    const construction = constructionsData.find((el) => el.id === id);
+    setConstructionModal(construction);
+    setModalActive(true);
+  };
+
+  const toggleActive = () => {
+    setModalActive(!modalActive);
+  };
 
   return (
     <>
+      <Modal active={modalActive} toggleActive={toggleActive}>
+        <ConstructionInfo construction={constructionModal} />
+      </Modal>
       <div className={style.orderTableRow}>
         <div className={nStyle}>Код</div>
         <div className={nStyle}>Объект строительства</div>
@@ -29,7 +47,11 @@ export default function OrdersTable() {
 
       {filtredData.map((construction) => {
         return (
-          <Construction key={construction.id} construction={construction} />
+          <Construction
+            key={construction.id}
+            construction={construction}
+            click={constructionNameClickHandler}
+          />
         );
       })}
     </>
